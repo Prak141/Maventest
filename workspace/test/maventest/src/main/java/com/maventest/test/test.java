db3ac40e41cdf7ecbdb705410fc267a0ca074839
prakash chidambaram <prakash.chidambaram@live.com>
2020-02-09 11:36:54 -0800

    Commiting Maventest and Unittest project

---
+++ workspace/test/maventest/src/main/java/com/maventest/test/test.java
@@ -0,0 +1,13 @@
+package com.maventest.test;
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println("This is test Method");
+	}
+	
+	public String validatetest() {
+		return "This is test Method";		
+	}
+
+}
