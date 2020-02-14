79b11f6648e558ac5fe5c3bfce16507a9d6e8398
prakash chidambaram <prakash.chidambaram@live.com>
2020-02-14 14:32:49 -0600

    Adding some additional test cases

---
+++ workspace/test/mavenunittest1/src/test/java/com/prakash/mavenunittest1/testcase3.java
@@ -0,0 +1,18 @@
+package com.prakash.mavenunittest1;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import com.maventest.test.*;
+
+
+public class testcase3 {
+
+	test test1 =  new test();
+	String result = test1.validatetest();
+	
+	@Test
+	public void validatetest() {
+		assertEquals(result,"This is test Method");	
+	}
+	
+}
---
+++ workspace/test/mavenunittest1/src/test/java/com/prakash/mavenunittest1/testcase7.java
@@ -0,0 +1,19 @@
+package com.prakash.mavenunittest1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.maventest.test.test;
+
+public class testcase7 {
+
+	test test1 =  new test();
+	String result = test1.validatetest();
+	
+	@Test
+	public void validatetest() {
+		assertEquals(result,"This is test Meth");	
+	}
+	
+}
